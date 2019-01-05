3063eb5e4b1dd277ba2fd844e35a23e1c977efb5
Charles Burton <charles.d.burton@gmail.com>
2019-01-05 13:52:52 -0700

    feat: more complete example

--- example/main.go
+++ example/main.go
@@ -7,8 +7,18 @@ import (
 )
 
 func main() {
-	_, err := serinit.GetDevices()
+	devices, err := serinit.GetDevices()
 	if err != nil {
 		log.Println(err)
+	}
+
+	for _, device := range devices {
+		_, err := device.SerialPort.Write([]byte("M105"))
+		if err != nil {
+			log.Println(err)
+		}
+		buf := make([]byte, 128)
+		device.SerialPort.Read(buf)
+		log.Println(string(buf))
 	}
 }
