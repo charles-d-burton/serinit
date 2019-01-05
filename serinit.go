1c8166624802f0de8916b19cf8a085606411245c
Charles Burton <charles.d.burton@gmail.com>
2019-01-05 13:32:06 -0700

    fix: parameterizied baud discovery

--- serinit.go
+++ serinit.go
@@ -100,41 +100,44 @@ func (device *SerialDevice) Close() error {
 func (device *SerialDevice) findBaudRate() (bool, error) {
 	fmt.Println("Testing common bauds")
 	for _, baud := range commonBauds {
-		sp, err := sers.Open(device.TTY)
+		valid, err := device.isBaudValid(baud)
 		if err != nil {
 			return false, err
 		}
-		device.SerialPort = sp
-		fmt.Printf("Setting baud to: %d\n", baud)
-		device.Baud = baud
-		found, err := testBaud(baud, device.SerialPort)
-		if err != nil {
-			return false, err
-		}
-		if found {
+		if valid {
 			return true, nil
 		}
-		sp.Close()
 	}
 	fmt.Println("Common bauds failed, attempting more comprehensive list")
 	for _, baud := range bauds {
-		sp, err := sers.Open(device.TTY)
+		valid, err := device.isBaudValid(baud)
 		if err != nil {
 			return false, err
 		}
-		device.SerialPort = sp
-		fmt.Printf("Setting baud to: %d\n", baud)
-		device.Baud = baud
-		found, err := testBaud(baud, device.SerialPort)
-		if err != nil {
-			return false, err
-		}
-		if found {
+		if valid {
 			return true, nil
 		}
-		sp.Close()
 	}
 	return false, nil
+}
+
+func (device *SerialDevice) isBaudValid(baud int) (bool, error) {
+	sp, err := sers.Open(device.TTY)
+	if err != nil {
+		return false, err
+	}
+	device.SerialPort = sp
+	fmt.Printf("Setting baud to: %d\n", baud)
+	device.Baud = baud
+	found, err := testBaud(baud, device.SerialPort)
+	if err != nil {
+		return false, err
+	}
+	if found {
+		return true, nil
+	} else {
+		return false, nil
+	}
 }
 
 //Create the connection then attempt to read from the serial port
