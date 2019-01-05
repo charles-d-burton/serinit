3b926da38f25af51572cb5c5a793da3535a367a5
Charles Burton <charles.d.burton@gmail.com>
2019-01-05 14:29:08 -0700

    test

--- example/main.go
+++ example/main.go
@@ -17,38 +17,43 @@ func main() {
 	}
 
 	for _, device := range devices {
+		readerChan := make(chan string, 1)
 		log.Println("Starting reader")
-		go readChannel(device.SerialPort)
+		go readChannel(device.SerialPort, readerChan)
 		log.Println("Requesting temperature")
 		_, err := device.SerialPort.Write([]byte("M105\n"))
 		log.Println("Request sent")
 		if err != nil {
 			log.Println(err)
 		}
+		file, err := os.Open("./hook.gcode")
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			value := scanner.Text()
+			if strings.HasPrefix(value, ";") {
+				log.Println("Comment: " + value)
+			} else {
+				devices[0].SerialPort.Write([]byte(value + "\n"))
+				retval := <-readerChan
+				log.Println(retval)
+
+			}
+			//fmt.Print(scanner.Text())
+		}
 	}
 
-	file, err := os.Open("./hook.gcode")
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		value := scanner.Text()
-		if strings.HasPrefix(value, ";") {
-			log.Println("Comment: " + value)
-		} else {
-			devices[0].SerialPort.Write([]byte(value + "\n"))
-		}
-		//fmt.Print(scanner.Text())
-	}
 }
 
-func readChannel(r io.Reader) {
+func readChannel(r io.Reader, reader chan string) {
 	for {
 		buf := make([]byte, 128)
 		r.Read(buf)
 		log.Println(string(buf))
+		reader <- string(buf)
 	}
 }
