df3311d1ff46cb89fbb6508271e8cd7d45dbddf5
Charles Burton <charles.d.burton@gmail.com>
2019-01-05 17:11:26 -0700

    test

--- example/main.go
+++ example/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"io"
 	"log"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/charles-d-burton/serinit"
+	"github.com/papertrail/go-tail/follower"
 )
 
 func main() {
@@ -20,9 +19,10 @@ func main() {
 
 	for _, device := range devices {
 		readerChan := make(chan string, 1)
+		writerChan := writeChannel(device.SerialPort)
 		log.Println("Starting reader")
-		go readChannel(device.SerialPort, readerChan)
-		go requestTemps(device.SerialPort)
+		go readChannel(device.TTY, readerChan)
+		//go requestTemps(device.SerialPort)
 		//log.Println("Requesting temperature")
 		//_, err := device.SerialPort.Write([]byte("M105\n"))
 		//log.Println("Request sent")
@@ -37,20 +37,18 @@ func main() {
 		defer file.Close()
 		scanner := bufio.NewScanner(file)
 		for scanner.Scan() {
-			value := scanner.Text()
-			if strings.HasPrefix(value, ";") {
-				log.Println("Comment: " + value)
-			} else {
-				//log.Println("Sending Command: " + value)
-				//device.SerialPort.Write([]byte(value + "\n"))
+			value := stripComments(scanner.Text())
+			if value != "" {
+				log.Println("Sending Command: " + value)
+				writerChan <- value
 				for {
 					select {
 					case retval := <-readerChan:
-						if strings.Contains(retval, "ok") {
+						if retval == "ok" {
 							log.Println("Breaking out of loop to send next command")
 							break
 						} else {
-							_, err := device.SerialPort.Write([]byte("M105\n"))
+							writerChan <- "M105\n"
 							if err != nil {
 								log.Println(err)
 							}
@@ -63,23 +61,46 @@ func main() {
 	}
 }
 
-func requestTemps(r io.Writer) error {
+/*func requestTemps(r io.Writer) error {
 	for {
 		fmt.Println("Requesting temps M105")
 		r.Write([]byte("M105\n"))
 		time.Sleep(time.Second)
 	}
+}*/
+
+func writeChannel(w io.Writer) chan string {
+	buf := make(chan string, 10)
+	go func() {
+		for {
+			select {
+			case line := <-buf:
+				_, err := w.Write([]byte(line))
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
+	}()
+	return buf
 }
 
-func readChannel(r io.Reader, reader chan string) {
-	buf := make([]byte, 128)
-	for {
-
-		len, err := r.Read(buf)
-		fmt.Println("Read: " + string(buf[:len]))
-		if err != nil {
-			log.Fatal(err)
-		}
-		reader <- string(buf[:len])
+func readChannel(deviceAddr string, reader chan string) {
+	t, err := follower.New(deviceAddr, follower.Config{
+		Whence: io.SeekEnd,
+		Offset: 0,
+		Reopen: true,
+	})
+	if err != nil {
+		log.Fatal(err)
 	}
+	for line := range t.Lines() {
+		reader <- line.String()
+	}
+}
+
+func stripComments(line string) string {
+	idx := strings.Index(line, ";")
+	command := strings.TrimSpace(string([]byte(line)[0:idx]))
+	return command + "\n"
 }
